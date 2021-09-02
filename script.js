24b39f8ff2adf7a087b1ade221a6d1b4f91a251d
Zach Yanez <zachyanez@gmail.com>
2021-09-02 18:09:10 -0500

    trying to add policy page'

--- script.js
+++ script.js
@@ -1,102 +1,89 @@
-currentTime = moment().format('MMMM Do YYYY, h:mm:ss a');
+currentTime = moment().format("MMMM Do YYYY, h:mm:ss a");
 var userLocation;
 
-$(".btn").on('click', function getLocation(){
-    if (navigator.geolocation){
-        navigator.geolocation.getCurrentPosition(showPosition)
-        $(".card-title").html(currentTime)
-       displaycurrentConditions()
-    }
+$(".btn").on("click", function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+    $(".card-title").html(currentTime);
+    displaycurrentConditions();
+  }
+});
 
-})
-
-function showPosition(position){
-    const userLocation = {
-        latitude: position.coords.latitude,
-        longitude: position.coords.longitude
-
-    };
-    console.log("User Location", userLocation)
-    getWeather(userLocation)
-
+function showPosition(position) {
+  const userLocation = {
+    latitude: position.coords.latitude,
+    longitude: position.coords.longitude,
+  };
+  console.log("User Location", userLocation);
+  getWeather(userLocation);
 }
 
 // Get Weather
-function getWeather(userLocation){
-    const apiKey = "8508d545d4e36b4c3c13e5d8071e830e";
+function getWeather(userLocation) {
+  const apiKey = "8508d545d4e36b4c3c13e5d8071e830e";
 
-    queryURL = `https://api.openweathermap.org/data/2.5/weather?lat=${userLocation.latitude}&lon=${userLocation.longitude}&appid=${apiKey}&units=imperial`
+  queryURL = `https://api.openweathermap.org/data/2.5/weather?lat=${userLocation.latitude}&lon=${userLocation.longitude}&appid=${apiKey}&units=imperial`;
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    })
-    .then(function(response){
-        console.log("OpenWeather Response", response)
-        const weatherData = {
-         currentHumidity : response.main.humidity,
-         currentWind : response.wind.speed,
-         currentTemp : response.main.temp,
-         currentConditions : response.weather[0].description,
-         icon : response.weather[0].icon,
-        }
-        console.log("Weather Data used", weatherData)
-        displaycurrentConditions(weatherData)
-
-    })
-
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  }).then(function (response) {
+    console.log("OpenWeather Response", response);
+    const weatherData = {
+      currentHumidity: response.main.humidity,
+      currentWind: response.wind.speed,
+      currentTemp: response.main.temp,
+      currentConditions: response.weather[0].description,
+      icon: response.weather[0].icon,
+    };
+    console.log("Weather Data used", weatherData);
+    displaycurrentConditions(weatherData);
+  });
 }
 
 // Check Hair Function
-function displaycurrentConditions(weatherData){
-    const iconimg = "https://openweathermap.org/img/wn/" + weatherData.icon + "@2x.png"
+function displaycurrentConditions(weatherData) {
+  const iconimg = "https://openweathermap.org/img/wn/" + weatherData.icon + "@2x.png";
 
-    if (weatherData.currentWind >= 20){
-        $(".output").html("It's pretty windy out there so...")
-        $(".humidity").html("Humidity : " + (weatherData.currentHumidity) + "%")
-        $(".tempature").html("Temperature : " + (weatherData.currentTemp))
-        $(".wind-speed").html("Wind Speed : " + (weatherData.currentWind) + " mph")
-        $(".conditions").html((weatherData.currentConditions))
-        $(".icon-img").attr('src', iconimg)
-        $(".card-button").html(" ")
-    }
-    else if (weatherData.currentHumidity >= 75)
-    {
-        $(".output").html("✨ Absolutely Not ✨")
-        $(".humidity").html("Humidity : " + (weatherData.currentHumidity) + "%")
-        $(".tempature").html("Temperature : " + (weatherData.currentTemp))
-        $(".wind-speed").html("Wind Speed : " + (weatherData.currentWind) + " mph")
-        $(".conditions").html((weatherData.currentConditions))
-        $(".icon-img").attr('src', iconimg)
-        $(".card-button").html(" ")
-    }
-    else if (weatherData.currentHumidity <= 74 && weatherData.currentHumidity >=70 )
-    {
-        $(".output").html("Probs Not")
-        $(".humidity").html("Humidity : " + (weatherData.currentHumidity) + "%")
-        $(".tempature").html("Temperature : " + (weatherData.currentTemp))
-        $(".wind-speed").html("Wind Speed : " + (weatherData.currentWind) + " mph")
-        $(".conditions").html((weatherData.currentConditions))
-        $(".icon-img").attr('src', iconimg)
-        $(".card-button").html(" ")
-    }
-    else if (weatherData.currentHumidity <= 69 && weatherData.currentHumidity >= 65)
-    {
-        $(".output").html("It's Feasible") 
-        $(".humidity").html("Humidity : " + (weatherData.currentHumidity) + "%")
-        $(".tempature").html("Temperature : " + (weatherData.currentTemp))
-        $(".wind-speed").html("Wind Speed : " + (weatherData.currentWind) + " mph")
-        $(".conditions").html((weatherData.currentConditions))
-        $(".icon-img").attr('src', iconimg)
-        $(".card-button").html(" ")
-    }
-    else if (weatherData.currentHumidity <= 64) {
-        $(".output").html("Lookin Pretty Good")
-        $(".humidity").html("Humidity : " + (weatherData.currentHumidity) + "%")
-        $(".tempature").html("Temperature : " + (weatherData.currentTemp))
-        $(".wind-speed").html("Wind Speed : " + (weatherData.currentWind) + " mph")
-        $(".conditions").html((weatherData.currentConditions))
-        $(".icon-img").attr('src', iconimg)
-        $(".card-button").html(" ")
-    }
-};
+  if (weatherData.currentWind >= 20) {
+    $(".output").html("It's pretty windy out there so...");
+    $(".humidity").html("Humidity : " + weatherData.currentHumidity + "%");
+    $(".tempature").html("Temperature : " + weatherData.currentTemp);
+    $(".wind-speed").html("Wind Speed : " + weatherData.currentWind + " mph");
+    $(".conditions").html(weatherData.currentConditions);
+    $(".icon-img").attr("src", iconimg);
+    $(".card-button").html(" ");
+  } else if (weatherData.currentHumidity >= 75) {
+    $(".output").html("✨ Absolutely Not ✨");
+    $(".humidity").html("Humidity : " + weatherData.currentHumidity + "%");
+    $(".tempature").html("Temperature : " + weatherData.currentTemp);
+    $(".wind-speed").html("Wind Speed : " + weatherData.currentWind + " mph");
+    $(".conditions").html(weatherData.currentConditions);
+    $(".icon-img").attr("src", iconimg);
+    $(".card-button").html(" ");
+  } else if (weatherData.currentHumidity <= 74 && weatherData.currentHumidity >= 70) {
+    $(".output").html("Probs Not");
+    $(".humidity").html("Humidity : " + weatherData.currentHumidity + "%");
+    $(".tempature").html("Temperature : " + weatherData.currentTemp);
+    $(".wind-speed").html("Wind Speed : " + weatherData.currentWind + " mph");
+    $(".conditions").html(weatherData.currentConditions);
+    $(".icon-img").attr("src", iconimg);
+    $(".card-button").html(" ");
+  } else if (weatherData.currentHumidity <= 69 && weatherData.currentHumidity >= 65) {
+    $(".output").html("It's Feasible");
+    $(".humidity").html("Humidity : " + weatherData.currentHumidity + "%");
+    $(".tempature").html("Temperature : " + weatherData.currentTemp);
+    $(".wind-speed").html("Wind Speed : " + weatherData.currentWind + " mph");
+    $(".conditions").html(weatherData.currentConditions);
+    $(".icon-img").attr("src", iconimg);
+    $(".card-button").html(" ");
+  } else if (weatherData.currentHumidity <= 64) {
+    $(".output").html("Lookin Pretty Good");
+    $(".humidity").html("Humidity : " + weatherData.currentHumidity + "%");
+    $(".tempature").html("Temperature : " + weatherData.currentTemp);
+    $(".wind-speed").html("Wind Speed : " + weatherData.currentWind + " mph");
+    $(".conditions").html(weatherData.currentConditions);
+    $(".icon-img").attr("src", iconimg);
+    $(".card-button").html(" ");
+  }
+}
